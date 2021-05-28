d246bc47a093fb64179bb575d9a3e7601e0d2849
dev-jeongwoo <dev.jeongwoo@gmail.com>
2021-05-28 08:53:41 -0300

    New Tweet Form

--- src/pages/PageAbout.vue
+++ src/pages/PageAbout.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page class="q-pa-lg">
-    <h4 class="q-mt-none q-mb-md">About Qwitter</h4>
+    <h4 class="q-mt-none q-mb-md">About Qweetter</h4>
     <div class="text-body1">
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus ut quis, mollitia impedit odit, inventore cum quo ipsa, quas omnis corrupti labore earum. Minus aspernatur fugit facere accusantium officiis facilis.</p>
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus ut quis, mollitia impedit odit, inventore cum quo ipsa, quas omnis corrupti labore earum. Minus aspernatur fugit facere accusantium officiis facilis.</p>
